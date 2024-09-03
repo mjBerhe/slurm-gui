1e404247aaf1aaad49f390ffb30471336286f879
matt <mjberhe123@hotmail.com>
2024-09-03 16:48:43 -0400

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,68 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+import { invoke } from '@tauri-apps/api'
+
+ function App() {
   const [count, setCount] = useState(0)
+  const [files, setFiles] = useState("initial value")
+
+  useEffect(() => {
+    const getFiles = async () => {
+      const files: string = await invoke("read_files")
+      setFiles(files)
+    }
+    getFiles()
+  }, [])
+
+  const allStrings = files.split(/\r?\n/);
+  console.log(`Total strings: ${allStrings.length}`);
+
+  const regex = /Job (\d+)/
+
+  const totalJobs = [...new Set(allStrings.map(x => {
+    const match = x.match(regex);
+    return match ? match[1] : null
+  }).filter(x => x!== null))].length
+
+  const allCompletedJobs = allStrings.filter(x => x.includes("completed successfully."))
+  console.log(`Completed Jobs: ${allCompletedJobs.length}`);
+  const uniqueCompletedJobs = [...new Set(allCompletedJobs)]
+
+  const resubmittedJobs = allStrings.filter(x => x.includes("resubmitted with ID"))
+
+  const failedJobs = allStrings.filter(x => x.includes("failed") || x.includes("does not end with: pALM successfully finishes the run"))
+  console.log(failedJobs.length)
+
+
+  const completeJobsStrings = allStrings.filter(x => x === "Complete jobs: 0.")
+  console.log(`Complete jobs string amount: ${completeJobsStrings.length}`)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React test!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="flex min-h-screen flex-col items-center text-white w-screen bg-gray-800">
+      <div className="container flex flex-col items-center px-4 py-16">
+        <div className="w-full text-center">Slurm-2090203</div>
+        <div className="">{totalJobs} Total Jobs</div>
+        <div className="grid grid-cols-3 gap-6 mt-12">
+          <div className="flex flex-col">
+            <span className="text-lg">Completed Jobs ({uniqueCompletedJobs.length})</span>
+            <div className="flex flex-col mt-2">
+              {uniqueCompletedJobs.map(x => <span key={x}>{x}</span>)}
+            </div>
+            
+          </div>
+          <div className='flex flex-col'>
+            <span className="text-lg">Resubmitted Jobs ({resubmittedJobs.length})</span>
+            <div className="flex flex-col mt-2">
+              {resubmittedJobs.map(x => <span key={x}>{x}</span>)}
+            </div>
+          </div>
+        </div>
+
+      </div>    
+    </main>
   )
 }
 
