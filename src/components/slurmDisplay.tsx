e5f5875ca97325c8fb1a5c731b1621091e67b7a3
matt <mjberhe123@hotmail.com>
2024-09-05 11:41:21 -0400

    summarize if fully complete

--- src/components/slurmDisplay.tsx
+++ src/components/slurmDisplay.tsx
@@ -1,3 +1,5 @@
+import { Check, X } from "lucide-react";
+
 export const SlurmDisplay: React.FC<{ file: { pathName: string; contents: string } }> = (
   props
 ) => {
@@ -9,15 +11,14 @@ export const SlurmDisplay: React.FC<{ file: { pathName: string; contents: string
 
   // splits file contents line by line
   const allStrings = contents.split(/\r?\n/);
-  console.log(`Total strings: ${allStrings.length}`);
 
-  const regex = /Job (\d+)/;
+  const jobRegex = /Job (\d+)/;
 
   const totalJobs = [
     ...new Set(
       allStrings
         .map((x) => {
-          const match = x.match(regex);
+          const match = x.match(jobRegex);
           return match ? match[1] : null;
         })
         .filter((x) => x !== null)
@@ -27,35 +28,54 @@ export const SlurmDisplay: React.FC<{ file: { pathName: string; contents: string
   const allCompletedJobs = allStrings.filter((x) =>
     x.includes("completed successfully.")
   );
-  console.log(`Completed Jobs: ${allCompletedJobs.length}`);
   const uniqueCompletedJobs = [...new Set(allCompletedJobs)];
+  const uniqueCompletedJobIds = uniqueCompletedJobs.map((x) => x.match(jobRegex)?.[1]);
 
   const resubmittedJobs = allStrings.filter((x) => x.includes("resubmitted with ID"));
 
-  const failedJobs = allStrings.filter(
-    (x) =>
-      x.includes("failed") ||
-      x.includes("does not end with: pALM successfully finishes the run")
+  // all jobs that failed
+  const failedJobs = allStrings
+    .filter(
+      (x) =>
+        x.includes("failed") ||
+        x.includes("does not end with: pALM successfully finishes the run")
+    )
+    .map((x) => {
+      return {
+        jobMessage: x,
+        jobNumber: x.match(jobRegex)?.[1],
+      };
+    });
+  // all jobs that failed and never completed after being resubmitted
+  const actuallyFailedJobs = failedJobs.filter(
+    (x) => !uniqueCompletedJobIds.includes(x.jobNumber)
   );
-  console.log(failedJobs.length);
 
-  const completeJobsStrings = allStrings.filter((x) => x === "Complete jobs: 0.");
-  console.log(`Complete jobs string amount: ${completeJobsStrings.length}`);
+  // const completeJobsStrings = allStrings.filter((x) => x === "Complete jobs: 0.");
+
+  const isComplete = uniqueCompletedJobs.length === totalJobs;
 
   return (
     <div className="flex flex-col items-center">
       <div className="w-full text-center">{fileName}</div>
       <div className="">{totalJobs} Total Jobs</div>
 
-      <div className="grid grid-cols-3 gap-6 mt-12 min-w-[800px]">
+      <div className="mt-4 flex gap-x-2">
+        <span>
+          {uniqueCompletedJobs.length} / {totalJobs} Completed
+        </span>
+        <div>{isComplete ? <Check color="green" /> : <X color="red" />}</div>
+      </div>
+
+      <div className="grid grid-cols-3 gap-6 mt-8 min-w-[800px]">
         <div className="flex flex-col border border-red-700 rounded-lg px-2 max-h-[500px] max-w-[300px] overflow-y-auto bg-red-500/10">
           <div className="flex flex-col items-center mt-4">
             <span className="text-xl font-bold">Failed Jobs</span>
-            <span className="font-medium">({failedJobs.length})</span>
+            <span className="font-medium">({actuallyFailedJobs.length})</span>
           </div>
           <div className="flex flex-col mt-4 items-center text-center">
-            {failedJobs.map((x) => (
-              <span key={x}>{x}</span>
+            {actuallyFailedJobs.map((x) => (
+              <span key={x.jobMessage}>{x.jobMessage}</span>
             ))}
           </div>
         </div>
